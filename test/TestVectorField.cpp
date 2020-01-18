c78416e560d2ecb36accdccd2127adb6aea0cbb8
morimoto0606 <morimoto0606@yahoo.co.jp>
2020-01-18 09:24:31 -0500

    add vecfield

--- test/TestVectorField.cpp
+++ test/TestVectorField.cpp
@@ -5,23 +5,24 @@
 #include "../src/VectorField.hpp"
 
 class VecFieldTest: public ::testing::Test {
+public:
     void SetUp() override {
         _a = 1;
         _b = 0.2;
         _beta = -.9;
         _rho = -0.7;
 
-        _sabr = sde::Sabr<double>(_a, _b, _beta, _rho);
+        _sabr = std::make_unique<sde::Sabr<double>>(_a, _b, _beta, _rho);
     }
-
+public:
     double _a;
     double _b;
     double _beta;
     double _rho;
-    sde::Sabr<double> _sabr;
+    std::unique_ptr<sde::Sabr<double>> _sabr;
 };
 
 
 TEST_F(VecFieldTest, getV0) {
-    auto f = _sabr.getV0();
+    auto f = _sabr->getV0();
 }
